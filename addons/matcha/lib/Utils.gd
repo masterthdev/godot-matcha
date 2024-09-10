6bb0d7e359e8995f08b19418be4774d79b20920d
masterthdev <64798939+masterthdev@users.noreply.github.com>
2024-09-10 20:42:52 +0300

    inject peer_id to generated random ids

--- addons/matcha/lib/Utils.gd
+++ addons/matcha/lib/Utils.gd
@@ -1,7 +1,13 @@
+static var peer_id := 0
 # Generate an random string with a certain length
-static func gen_id(len:=20, charset:="0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ") -> String:
+static func gen_id(len:=20, charset:="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ") -> String:
 	var word: String
 	var n_char = len(charset)
 	for i in range(len):
 		word += charset[randi()% n_char]
+	
+	if peer_id == 0: peer_id = randi_range(2,9999999999)
+	var peer_id_str := str(peer_id)
+	for i in range(peer_id_str.length()):
+		word[i] = peer_id_str[i]
 	return word
